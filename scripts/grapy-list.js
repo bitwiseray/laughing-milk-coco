fcaff5fc99acd6992bb2a62c4dd996ee25827648
pbawane899 <pbawane899@gmail.com>
2025-05-09 20:47:12 +0000

    keep things seprated (not encouarging divorce)

---
+++ scripts/grapy-list.js
@@ -0,0 +1,26 @@
+VANTA.TOPOLOGY({
+    el: "#hero",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color:  0xFF94A8,
+    backgroundColor: 0xffffff
+});
+
+VANTA.GLOBE({
+    el: "#about",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xFAF9F6,
+    color2: 0xFF94A8,
+    backgroundColor: 0x0E1111
+  })
